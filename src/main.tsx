2addda1af9cbfab532c185086fa309dc11e39aae
stratacume <stratacume@gmail.com>
2026-05-23 11:12:36 +1200

    restore app router and layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-mport React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import AppLayout from '../AppLayout';
