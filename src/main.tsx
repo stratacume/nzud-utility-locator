ab183e06850ceb09bd00e08da0df273d2ce0cb83
stratacume <stratacume@gmail.com>
2026-05-23 10:53:22 +1200

    restore app router entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import AppLayout from '../AppLayout';
+import Index from '../App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppLayout />
-    </BrowserRouter>
+    <Index />
   </React.StrictMode>,
 );
