7d53b0725b44a034201c1ce95001e8b530a28bef
stratacume <stratacume@gmail.com>
2026-05-23 10:37:54 +1200

    commit directly to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import AppLayout from '../AppLayout';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppLayout />
+    <BrowserRouter>
+      <AppLayout />
+    </BrowserRouter>
   </React.StrictMode>,
 );
