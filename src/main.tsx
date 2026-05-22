8cd47e677c59e0a04448fef14127cace759ff959
stratacume <stratacume@gmail.com>
2026-05-23 11:34:29 +1200

    Change import path for AppLayout to .tsx extension

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import AppLayout from './components/AppLayout';
+import AppLayout from './components/AppLayout.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
