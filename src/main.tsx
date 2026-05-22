f4fd6fb12c9b63a2cf0d5dcbe9aac28c23956396
stratacume <stratacume@gmail.com>
2026-05-23 10:27:33 +1200

    commit directly to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppLayout from './components/AppLayout';
+import AppLayout from '../AppLayout';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
