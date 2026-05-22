1e2782940200455e281b50fbce05f22900204d56
stratacume <stratacume@gmail.com>
2026-05-23 09:22:47 +1200

    commit directly to main

--- AppLayout.tsx
+++ AppLayout.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Navigation from './Navigation';
 import HeroSection from './HeroSection';
-import PricingSection from './src/components/PricingSection.tsx';
 import Logo from './Logo';
 import NZUDTextLogo from './NZUDTextLogo';
 import CertifiedLocatorLogo from './CertifiedLocatorLogo';
@@ -35,7 +34,7 @@ const AppLayout: React.FC = () => {
         onQuoteClick={scrollToPricing}
         onBookClick={scrollToPricing}
       />
-      <PricingSection />
+      
 
 
 
