780680667963da523750182e28ecffec40a0aef2
stratacume <stratacume@gmail.com>
2026-05-23 12:51:05 +1200

    remove missing emailService import

--- src/components/PricingSection.tsx
+++ src/components/PricingSection.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { supabase } from '@/lib/supabase';
-import { sendBookingConfirmationEmail, generateSignedDocumentLinks } from '@/lib/emailService';
 import AdditionalInfo from '../pricing/AdditionalInfo';
 import BookingCard from '../pricing/BookingCard';
 
