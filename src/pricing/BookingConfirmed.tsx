a99c98011efb51027bf8be4aae7173c4b2b2d852
stratacume <stratacume@gmail.com>
2026-05-23 13:38:59 +1200

    Add BookingConfirmed and FileUpload components

---
+++ src/pricing/BookingConfirmed.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BookingConfirmed: React.FC<any> = () => {
+  return null;
+};
+
+export default BookingConfirmed;
+
+
+import React from 'react';
+
+const FileUpload: React.FC<any> = () => {
+  return null;
+};
+
+export default FileUpload;
